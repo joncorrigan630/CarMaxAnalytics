bc479e819d350b3a8509d31330478f510767101e
Jon Corrigan <joncorrigan630@gmail.com>
2021-06-23 16:29:10 -0400

    Update Carmax Data - Create Tables.sql

--- Carmax Data - Create Tables.sql
+++ Carmax Data - Create Tables.sql
@@ -2,7 +2,7 @@ CREATE DATABASE carmax;
 USE carmax; 
 
 CREATE TABLE carmax_main (
-	insert_num INT NOT NULL, 
+    insert_num INT NOT NULL, 
     purchase_make VARCHAR(25), 
     purchase_model VARCHAR(25), 
     purchase_vehicle_year YEAR, 
@@ -21,8 +21,8 @@ CREATE TABLE carmax_main (
 );
 
 CREATE TABLE purchase_info AS
-	SELECT insert_num AS purchase_id, 
-	purchase_price, trade_in, vehicle_financing, vehicle_warranty_used,
+    SELECT insert_num AS purchase_id, 
+    purchase_price, trade_in, vehicle_financing, vehicle_warranty_used,
     customer_previous_purchase AS previous_purchase, 
     subsequent_purchases 
 FROM carmax_main; 
@@ -31,7 +31,7 @@ ALTER TABLE purchase_info
 ADD PRIMARY KEY (purchase_id); 
 
 CREATE TABLE vehicle_info AS
-	SELECT insert_num AS vehicle_id, 
+    SELECT insert_num AS vehicle_id, 
     purchase_make AS vehicle_make, 
     purchase_model AS vehicle_model, 
     purchase_vehicle_year AS vehicle_year
@@ -41,7 +41,7 @@ ALTER TABLE vehicle_info
 ADD PRIMARY KEY (vehicle_id); 
 
 CREATE TABLE customer_info AS 
-	SELECT insert_num AS customer_id, 
+    SELECT insert_num AS customer_id, 
     customer_age, customer_income, customer_gender, 
     customer_distance_to_dealer, 
     post_purchase_satisfaction AS customer_satisfaction
@@ -49,6 +49,3 @@ FROM carmax_main;
 
 ALTER TABLE customer_info 
 ADD PRIMARY KEY (customer_id); 
-
-    
-    
