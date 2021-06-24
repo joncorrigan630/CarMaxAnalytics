1b1d83462c150e33642d1dae10cb4c62f1261ea8
Jon Corrigan <joncorrigan630@gmail.com>
2021-06-23 22:09:52 -0400

    Update Carmax Data - Create Views.sql

--- Carmax Data - Create Views.sql
+++ Carmax Data - Create Views.sql
@@ -1,6 +1,6 @@
 CREATE VIEW purchases AS
-SELECT purchase_price AS purchase_price_bins,
-	   AVG(purchase_price) + 4999/2 AS purchase_price_midrange, 
+       SELECT purchase_price AS purchase_price_bins,
+       AVG(purchase_price) + 4999/2 AS purchase_price_midrange, 
        COUNT(purchase_price) AS purchase_price_count, 
        SUM(trade_in) AS trade_in_vehicles, 
        SUM(vehicle_financing) AS vehicles_financed, 
@@ -13,17 +13,17 @@ GROUP BY purchase_price
 ORDER BY purchase_price DESC; 
 
 CREATE VIEW vehicles AS
-SELECT COUNT(insert_num) AS total_vehicles_sold, 
-	   AVG(purchase_price) + 4999/2 AS average_purchase_price,
-	   purchase_make AS vehicle_make, purchase_model AS vehicle_model, 
+       SELECT COUNT(insert_num) AS total_vehicles_sold, 
+       AVG(purchase_price) + 4999/2 AS average_purchase_price,
+       purchase_make AS vehicle_make, purchase_model AS vehicle_model, 
        purchase_vehicle_year AS vehicle_year
 FROM carmax_main 
 GROUP BY purchase_make, purchase_model, purchase_vehicle_year
 ORDER BY purchase_make DESC, total_vehicles_sold DESC; 
 
 CREATE VIEW customers AS
-SELECT COUNT(insert_num) AS total_purchases, 
-	   AVG(purchase_price) + 4999/2 AS average_purchase_price,
+       SELECT COUNT(insert_num) AS total_purchases, 
+       AVG(purchase_price) + 4999/2 AS average_purchase_price,
        AVG(customer_income) AS average_customer_income,
        customer_gender
 FROM carmax_main 
